c2bd7577593d21e95bf90c0dbafbfe9c2c53457a
neko4-admin <2015520559@qq.com>
2025-08-10 15:23:43 +0800

    把数学公式变成基本的matlab代码版本

--- hybridCG_algorithm.m
+++ hybridCG_algorithm.m
@@ -1,1 +1,37 @@
-%实现论文中的算法
+%论文中的算法
+
+dim = 4;
+iteration = 5;
+
+glist = rand( dim , iteration);
+glist = [glist,rand(dim,1)];
+disp(glist)
+
+dlist = rand( dim , iteration);
+dlist = [dlist,rand(dim,1)];
+disp(dlist)
+
+k = 2;
+
+beta_MHS = glist(:,k)'* (glist(:,k)- glist(:,k-1))/ ((glist(:,k)- glist(:,k-1))'* dlist(:,k-1))*...
+           (1- (glist(:,k)'* dlist(:,k-1))^2/ (norm(glist(:,k))^2* norm(dlist(:,k-1))^2 ));
+beta_DY = norm(glist(:,k))^2/ ((glist(:,k)- glist(:,k-1))'* dlist(:,k-1));
+disp(beta_MHS)
+disp(beta_DY)
+
+beta_N = max(0,min(beta_DY,beta_MHS));
+disp(beta_N)
+
+d_k = -(1+ beta_N* (glist(:,k)'* dlist(:,k-1))/ norm(glist(:,k))^2)* glist(:,k )+ beta_N*dlist(:,k-1);
+disp(d_k)
+
+
+
+
+
+% for beta_HS = gr
+%     fprintf('beta_HS = %d\n', beta_HS);
+% end
+
+
+
