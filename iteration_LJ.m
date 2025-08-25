8962a86c999dd6647c59e4ff8f6da3022e6c0ca2
neko4-admin <2015520559@qq.com>
2025-08-25 16:26:58 +0800

    在LJ力场中测试线搜索代码

--- iteration_LJ.m
+++ iteration_LJ.m
@@ -1,4 +1,4 @@
-%嵌入原子势
+%LJ势能
 
 clear
 set(groot , 'DefaultAxesFontName', '宋体', 'DefaultAxesFontSize', 15);  % 设置坐标轴标签的默认字体和大小
@@ -19,16 +19,11 @@ pointsNum = 100;
 n = dimension*pointsNum;%总维数
 
 %力场参数
-r0Prm = 2.556;
-rcPrm = 5;
-E0Prm = 3.54;
-Phi0Prm = 4.86;
-alphaPrm = 5.9;
-betaPrm = 5.85;
-gammaPrm = 6.3;
+epsilon = 1;
+sigma = 3.46;
 
 %初始粒子坐标取值范围
-iniRange = r0Prm* (0.5+ (3*pointsNum/ (4* pi* sqrt(2)))^ (1/3) );
+iniRange = sigma* 2^(1/6)* (0.5+ (3*pointsNum/ (4* pi* sqrt(2)))^ (1/3) );
 
 % 生成随机点的空坐标
 pointsList = zeros(n,1);
@@ -43,7 +38,7 @@ pointsList(1:dimension) = randomPoint;
 for i = 2:pointsNum
     randomPoint = (2*rand(dimension,1)-1)*iniRange;
     minDis = distence(pointsList,randomPoint,dimension);
-    while norm(randomPoint) > iniRange || minDis(1) < 0.9* r0Prm
+    while norm(randomPoint) > iniRange || minDis(1) < sigma
         randomPoint = (2*rand(dimension,1)-1)*iniRange;
         minDis = distence(pointsList,randomPoint,dimension);
         %fprintf('太近了，重开\n')
@@ -51,15 +46,13 @@ for i = 2:pointsNum
     pointsList((i-1)*dimension+1:i*dimension) = randomPoint;
 end
 
-% load('pointsList_1.mat');
+load('pointsList_1.mat');
 save(['pointsList_',num2str(systemNum)],'pointsList')
 
 
-neighbor = neighborMatrix(pointsList,rcPrm,dimension,pointsNum);
-eleDensity = electrondensity(pointsList,r0Prm,betaPrm,neighbor,dimension,pointsNum);
-energy = potential(pointsList,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
-gr = gradient(pointsList,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
-dr = - gr;
+energy = potential(epsilon,sigma,pointsList,dimension,pointsNum);
+gr = gradient(epsilon,sigma,pointsList,dimension,pointsNum);%初始点梯度
+dr = - gr;%初始搜索方向为负梯度方向
 
 %用数组记录grad,direction,energy,h,方向变化间隔
 k = 1;
@@ -68,10 +61,10 @@ glist = gr;
 dlist = dr;
 elist = energy;
 %搜索步长
-h = max(abs(gr))/4;
+h = max(abs(gr))/100;
 %fprintf('初始步长: %.4f\n',h)
-% hlist = h;
-% section = 1;
+%hlist = h;
+%section = 1;
 nantest = 0; %检测NaN报错
 maxMove = 0.1;
 
@@ -88,26 +81,18 @@ w = 0;
 times = 0;
 timeTotal = 0;
 
-while norm(gr) > 0.005 && times < 500
+while norm(gr) > 0.005 && times < 1000
     times = times + 1;
     %fprintf('\n第 %d 个点,\n',times);
 
     %三阶插值
     t1 = 0;
-    neighbor = neighborMatrix(pointsList,rcPrm,dimension,pointsNum);
-    eleDensity = electrondensity(pointsList,r0Prm,betaPrm,neighbor,dimension,pointsNum);
-    f1 = potential(pointsList,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
+    f1 = potential(epsilon,sigma,pointsList,dimension,pointsNum);
     g1 = gr'*dr;
     %fprintf('t1: %.8f, f1: %.4f, g1: %.4f, \n',t1,f1,g1);
 
-    if h > 5
-        h = 1;
-        fprintf('change the step - h\n');
-    end
-
     limit = 0;
     while (abs(g1) > accu && limit < 10)|| limit == 0
-
         limit = limit + 1;
 
         if g1 < 0
@@ -117,13 +102,10 @@ while norm(gr) > 0.005 && times < 500
         end
 
         t2 = t1 + h;
-        pointsListMid = pointsList + t2*dr;
-        neighbor = neighborMatrix(pointsListMid,rcPrm,dimension,pointsNum);
-        eleDensity = electrondensity(pointsListMid,r0Prm,betaPrm,neighbor,dimension,pointsNum);
-        f2 = potential(pointsListMid,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
-        g2 =  gradient(pointsListMid,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum)'*dr;
+        f2 = potential(epsilon,sigma,pointsList+t2*dr,dimension,pointsNum);
+        g2 = gradient(epsilon,sigma,pointsList+t2*dr,dimension,pointsNum)'*dr;
         %fprintf('t2: %.8f, f2: %.4f, g2: %.4f,\n',t2,f2,g2)
-
+        
         if abs(g2) < accu %&& abs(g2) < abs(g1)
             t1 = t2*2^sign(g1*g2);
             f1 = f2;
@@ -132,8 +114,8 @@ while norm(gr) > 0.005 && times < 500
             %fprintf('点： %.8f , f: %.6f , g: %.6f \n',t1,f1,g1);
             timeTotal = timeTotal + 1;
             elist = [elist,f1];
-%             h = t1;
-%             hlist = [hlist,h];
+            %h = t1;
+            %hlist = [hlist,h];
             break;
         end
 
@@ -150,12 +132,9 @@ while norm(gr) > 0.005 && times < 500
                    (1- ( (1-condition_t)*g2 + condition_t*g1 + w+ z)/...
                    ((-1)^condition_t*(g2- g1) + 2*w));
 
-            pointsListMid = pointsList + tNew*dr;
-            neighbor = neighborMatrix(pointsListMid,rcPrm,dimension,pointsNum);
-            eleDensity = electrondensity(pointsListMid,r0Prm,betaPrm,neighbor,dimension,pointsNum);
-            fNew = potential(pointsListMid,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
-            gNew =  gradient(pointsListMid,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum)'*dr;
-            %fprintf('求解出的新点：aNew: %.8f, fNew: %.4f, gNew: %.4f, \n',tNew,fNew,gNew);
+            fNew = potential(epsilon,sigma,pointsList+tNew*dr,dimension,pointsNum);
+            gNew = gradient(epsilon,sigma,pointsList+tNew*dr,dimension,pointsNum)'*dr;
+            %fprintf('求解出的新点：a1: %.8f, f1: %.4f, g1: %.4f, \n',tNew,fNew,gNew);
 
             if ( t1<tNew && tNew<t2 )||( t1>tNew && tNew>t2 )
                 if gNew * g1 > 0 || abs(gNew) < abs(g1)/2
@@ -175,11 +154,8 @@ while norm(gr) > 0.005 && times < 500
             else
                 h = h/4;
                 t1 = (t1+t2)/2;
-                pointsListMid = pointsList + t1*dr;
-                neighbor = neighborMatrix(pointsListMid,rcPrm,dimension,pointsNum);
-                eleDensity = electrondensity(pointsListMid,r0Prm,betaPrm,neighbor,dimension,pointsNum);
-                f1 = potential(pointsListMid,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
-                g1 =  gradient(pointsListMid,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum)'*dr;
+                f1 = potential(epsilon,sigma,pointsList+t1*dr,dimension,pointsNum);
+                g1 =  gradient(epsilon,sigma,pointsList+t1*dr,dimension,pointsNum)'*dr;
                 %fprintf('三次插值出错，取中点\n')
             end
         elseif abs(g2) < 10*abs(g1)
@@ -190,11 +166,11 @@ while norm(gr) > 0.005 && times < 500
             %fprintf('前进,而且步长 h 太短了…… %.5f\n',h);
         else
             h = h/6;
-            %fprintf('梯度异常返回, h: %.5f\n',h);
+            %fprintf('梯度异常，返回\n');
         end
         timeTotal = timeTotal + 1;
         elist = [elist,f1];
-%         hlist = [hlist,h];
+        %hlist = [hlist,h];
     end
 
     if t1 == 0
@@ -202,11 +178,10 @@ while norm(gr) > 0.005 && times < 500
     end
 
     %fprintf('总循环数:%d\n',timeTotal)
-%     fprintf('t1:%.4f\n',t1)
 
     h = abs(t1);
     k = k + 1;
-%     section = [section,timeTotal];
+    %section = [section,timeTotal];
 
     %限制最大位移
     %fprintf('不限制下最大的移动距离: %.4f\n',max(abs(t1*dr)));
@@ -230,14 +205,11 @@ while norm(gr) > 0.005 && times < 500
 %         targetPi = distence(pointsList,farPi,dimension);
 %         targetPi = targetPi(2);
 %         targetPi = pointsList((targetPi-1)*dimension+1:targetPi*dimension);
-%         pointsList((indixi-1)*dimension+1:indixi*dimension) = targetPi + r0Prm*(farPi-targetPi)/norm(farPi-targetPi);
+%         pointsList((indixi-1)*dimension+1:indixi*dimension) = targetPi + 1.15*sigma*(farPi-targetPi)/norm(farPi-targetPi);
 %         %fprintf('移动了 %d 个原子\n',i)
 %     end
 
-    neighbor = neighborMatrix(pointsList,rcPrm,dimension,pointsNum);
-    eleDensity = electrondensity(pointsList,r0Prm,betaPrm,neighbor,dimension,pointsNum);
-    gr = gradient(pointsList,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
-
+    gr = gradient(epsilon,sigma,pointsList,dimension,pointsNum);
     if any(isnan(gr))
         %disp('gr = NaN, 退出\n');
         break;
@@ -256,15 +228,13 @@ while norm(gr) > 0.005 && times < 500
     dr = -(1+ beta_N* (g_k'* d_k1)/ norm(g_k)^2)* g_k+ beta_N*d_k1;
     dlist = [dlist,dr];
 
-
 end
 
-neighbor = neighborMatrix(pointsList,rcPrm,dimension,pointsNum);
-eleDensity = electrondensity(pointsList,r0Prm,betaPrm,neighbor,dimension,pointsNum);
-energy = potential(pointsList,r0Prm,E0Prm,Phi0Prm,alphaPrm,betaPrm,gammaPrm,neighbor,eleDensity,dimension,pointsNum);
+energy = potential(epsilon,sigma,pointsList,dimension,pointsNum);
 fprintf('最终点能量：%.4f,梯度模长：%.6f\n',energy,norm(gr))
 
 recordList = [energy;times;timeTotal;nantest];
+filename = sprintf('_%.4f_%d_%d_%d.mat', energy, times, timeTotal,nantest);
 
 fileID = fopen('Result.txt','w');
 fprintf(fileID,'%d_%.4f_%d_%d_%d\n', systemNum, energy, times, timeTotal,nantest);
@@ -272,117 +242,40 @@ fprintf(fileID,'%d_%.4f_%d_%d_%d\n', systemNum, energy, times, timeTotal,nantest
 save(['Result_',num2str(systemNum)],'pointsList','elist','nantest','energy','times','timeTotal')
 
 
+% % 原始能量下降曲线
+% figure('Position', [40, 680, 700, 600]);
+% xlist= 1:length(elist);
+% plot(xlist,elist,xlist,elist,'r.',section,elist(section),'kx','MarkerSize',11)
+% xlabel('迭代次数'), ylabel('体系能量');
+% title('原始能量下降曲线')
+% ylim([-550,-300])
 
 % 能量下降曲线
 figure('Position', [40, 680, 700, 600]);xlist= 1:length(elist);
 elist2 = log(elist-min(elist)+0.001)/log(10);
 plot(xlist,elist2,xlist,elist2,'r.')
-xlabel(['迭代次数,搜索方向改变',num2str(times),'次']), ylabel('体系能量');
+xlabel(['迭代次数, 总点数：',num2str(times)]), ylabel('体系能量');
 title(['能量下降曲线，总循环数：',num2str(timeTotal)])
 
 % 画出所有点的坐标
 figure('Position', [740, 680, 700, 600]);
 midList = 1:3:n;
-%plot3(pointsList(midList),pointsList(midList+1),pointsList(midList+2),'.');
+% plot3(pointsList(midList),pointsList(midList+1),pointsList(midList+2),'.');
 scatter3(pointsList(midList), pointsList(midList+1), pointsList(midList+2), 20, 'b', 'filled'); 
-title('所有点的位置图');
+title('所有点的位置图')
 
 fprintf('finish!\n')
 
+% % 步长的变化
+% figure('Position', [50, 0, 2800, 600]);
+% xlist= 1:length(hlist);
+% normglist = sqrt(sum(glist.^2, 1))/pointsNum;
+% plot(xlist,hlist,xlist,hlist,'r.',section,hlist(section),'kx',section,normglist,'bo','MarkerSize',11)
+% xlabel('迭代次数'), ylabel('搜索步长');
+% title('步长 h 变化曲线')
+% ylim([0,0.1])
 
-%writeVestaFile('output.vesta',pointsList,dimension,pointsNum)
-
-
-
-%所有近邻
-function out = neighborMatrix(pointsList,rc,dim,num)
-    out = zeros(num);
-    for i = 1:num
-        ri = pointsList((i-1)*dim+1:i*dim);
-        for j = i+1:num
-            if norm(ri-pointsList((j-1)*dim+1:j*dim)) <= rc
-                out(i,j) = 1;
-                out(j,i) = 1;
-            end
-        end
-    end
-end
-
-
-%背景电子密度
-function out = electrondensity(pointsList,r0,beta,neighbor,dim,num)
-    out = zeros(num,1);
-    for i = 1:num
-        mid = 0;
-        ri = pointsList((i-1)*dim+1:i*dim);
-        rineighbor = neighbor(i,:);
-        rineighbor = find(rineighbor);
-        if ~isempty(rineighbor)
-            for j = rineighbor
-                mid = mid + exp( - beta* (norm(ri-pointsList((j-1)*dim+1:j*dim))/ r0 - 1) );
-            end
-            out(i) = mid/12;
-        end
-    end
-end
-
-
-
-%势能
-function out = potential(pointsList,r0,E0,Phi0,alpha,beta,gamma,neighbor,eleDensity,dim,num)
-    out = 0;
-
-    %嵌入能
-    for i = 1:num
-        rhoi = eleDensity(i);
-        if rhoi ~= 0
-            out = out - E0* (1 - (alpha/beta)* log(rhoi))* rhoi^(alpha/beta) - 6* Phi0 * rhoi^(gamma/beta);
-        end
-    end
-    
-    %排斥能
-    for i = 1:num-1
-        ri = pointsList((i-1)*dim+1:i*dim);
-        indices = find(neighbor(i,i+1:num)) + i;
-        if ~isempty(indices)
-            for j = indices 
-                out = out + Phi0* exp( - gamma* (norm(ri-pointsList((j-1)*dim+1:j*dim))/ r0 - 1) );
-            end
-        end
-    end
-end
-
-%梯度
-function out = gradient(pointsList,r0,E0,Phi0,alpha,beta,gamma,neighbor,eleDensity,dim,num)
-    out = zeros(dim*num,1);
-    mid0 = zeros(dim,1);
-        
-    for i = 1:num
-
-        rhoi =  eleDensity(i);
-        ri = pointsList((i-1)*dim+1:i*dim);
-        mid = mid0;
-
-        if rhoi ~= 0
-            rineighbor = neighbor(i,:);
-            rineighbor = find(rineighbor);
-
-            for j = rineighbor
-                rhoj = eleDensity(j);
-                rirj = ri - pointsList((j-1)*dim+1:j*dim);
-                %嵌入能的导数
-                mid = mid + (beta/ (12* r0))*...
-                            ( - (alpha/beta)^2* E0* log(rhoi)* rhoi^ (alpha/beta- 1)+ 6* (gamma/beta)* Phi0* rhoi^ (gamma/beta- 1)...
-                              - (alpha/beta)^2* E0* log(rhoj)* rhoj^ (alpha/beta- 1)+ 6* (gamma/beta)* Phi0* rhoj^ (gamma/beta- 1)...
-                            )* exp( -beta* (norm(rirj)/r0 - 1) )* rirj/ norm(rirj) ;
-                %排斥能的导数
-                mid = mid - (gamma/r0)* Phi0* exp( -gamma* (norm(rirj)/r0 - 1) )* rirj/ norm(rirj);
-            end
-        end
-
-        out((i-1)*dim+1:i*dim) = mid;
-    end
-end
+% writeVestaFile('output.vesta',pointsList,dimension,pointsNum)
 
 
 %粒子群中，离某个位置最近的的原子 的序号和距离
@@ -415,6 +308,38 @@ function out = averageDistence2List(pointsList,dim,num)
         out(i) = mid/num;
     end
 end
+
+%势能
+function out = potential(epsi,sigma,plist,dim,num)
+out = 0;
+for i = 0:num-2
+    ri = plist(i*dim+1:(i+1)*dim);
+    for j = i+1:num-1
+        sigmarj = sigma/norm(ri-plist(j*dim+1:(j+1)*dim));
+        out = out + 4* epsi* (sigmarj^12-sigmarj^6);
+    end
+end
+end
+
+
+
+%梯度
+function out = gradient(epsi,sigma,plist,dim,num)
+out = zeros(dim*num,1);
+mid0 = zeros(dim,1);
+for i = 0:num-1
+    ri = plist(i*dim+1:(i+1)*dim);
+    mid = mid0;
+    for j = 0:num-1
+        if j ~= i
+            rirj = ri - plist(j*dim+1:(j+1)*dim);
+            mid = mid + 4* epsi* rirj * (-12*sigma^12/norm(rirj)^14 + 6*sigma^6/norm(rirj)^8);
+        end
+    end
+    out(i*dim+1:(i+1)*dim) = mid;
+end
+end
+
 
 %将粒子坐标输出成Vesta文件格式
 function writeVestaFile(filename,pointsList,dimension,pointsNum)
@@ -461,7 +386,5 @@ function writeVestaFile(filename,pointsList,dimension,pointsNum)
     
     fclose(fileID);
 end
-
-
 
 
